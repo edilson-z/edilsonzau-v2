e830eda31861d2832b580e8806b75ef9a939c4a0
edilson-z <dannibuga@gmail.com>
2026-09-18 18:54:12 +0200

    header html skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,8 @@ function App() {
   return (
     <div className='shell'>
       <div className='header'>
-        Edilson Zau Portfolio
+        <h1>Edilson Zau</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae ratione temporibus sequi perferendis molestias soluta. Cumque impedit a nostrum accusamus, blanditiis amet veritatis excepturi dolore necessitatibus ab incidunt aut cum.</p>
       </div>
     </div>
   )
