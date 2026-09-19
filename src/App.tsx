0176759eda670abb4faa9e3d00b8468b10ccd024
edilson-z <dannibuga@gmail.com>
2026-09-19 13:52:31 +0200

    Headear contents centerd

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,8 @@ function App() {
   return (
     <div className='shell'>
       <div className='header'>
-        <h1>Edilson Zau</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae ratione temporibus sequi perferendis molestias soluta. Cumque impedit a nostrum accusamus, blanditiis amet veritatis excepturi dolore necessitatibus ab incidunt aut cum.</p>
+        <h1>Hello, I'm <span>Edilson Zau</span></h1>
+        <p>I'm currently working as a Freelance Software Developer. I don't really know what I'm doing. I use to think I was a great developer but now, not so much. I'm always willing to learn, and I do my best in every project I work on.</p>
       </div>
     </div>
   )
