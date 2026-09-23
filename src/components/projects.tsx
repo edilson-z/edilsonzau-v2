871cea6199d2c093c80ec74bd9e304bb41867db0
edilson-z <dannibuga@gmail.com>
2026-09-23 19:56:35 +0200

    Projects skeleton created

---
+++ src/components/projects.tsx
@@ -0,0 +1,15 @@
+export default function Projects() {
+    return (
+        <div>
+            <h2>Projects</h2>
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+        </div>
+    )
+}
