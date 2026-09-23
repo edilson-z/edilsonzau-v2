2abe6175358fc1b48376c7d43c173b6e975d3cc3
edilson-z <dannibuga@gmail.com>
2026-09-23 21:41:07 +0200

    v1 project card created

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,7 +1,29 @@
+import './component-styles/Projects.css'
+
 export default function Projects() {
     return (
         <div>
             <h2>Projects</h2>
+            <div className="project-cards">
+                <div className="project-card">
+                    <img src='https://placehold.co/400x200' />
+                    <div className='project-card-container'>
+                        <h3>SMS Agri Chatbot</h3>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.  velit amet culpa repellendus aperiam assumenda quidem vitae</p>
+                        <span className="project-tag">Python</span>
+                            <span className="project-tag">Flask</span>
+                            <span className="project-tag">OpenAI</span>
+                            <span className="project-tag">Twillio</span>
+                            <span className="project-tag">MongoDB</span>
+                            <span className="project-tag">NumPy</span>
+                            <span className="project-tag">Pandas</span>
+                            <span className="project-tag">Jupyter</span>
+                            <span className="project-tag">Git</span>
+                            <span className="project-tag">NGROK</span>
+                    </div>
+                </div>
+                <div className="project-card">Hydro Tent</div>
+            </div>
             <br />
             <br />
             <br />
