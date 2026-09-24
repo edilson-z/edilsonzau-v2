ad26640cbfc9d440836ca0663c4d717446567418
edilson-z <dannibuga@gmail.com>
2026-09-24 16:11:17 +0200

    SMS photos uploaded, simple styling added for image

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -6,7 +6,9 @@ export default function Projects() {
             <h2>Projects</h2>
             <div className="project-cards">
                 <div className="project-card">
-                    <img src='https://placehold.co/400x200' />
+                    <div className='image-container'>
+                        <img src="./sms.png" />
+                    </div>
                     <div className='project-card-container'>
                         <h3>SMS Agri Chatbot</h3>
                         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.  velit amet culpa repellendus aperiam assumenda quidem vitae</p>
@@ -27,16 +29,14 @@ export default function Projects() {
                     <div className='project-card-container'>
                         <h3>Hydro Tent</h3>
                         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.  velit amet culpa repellendus aperiam assumenda quidem vitae</p>
+                        <span className="project-tag">Java</span>
+                        <span className="project-tag">Kotlin</span>
+                        <span className="project-tag">Spring</span>
+                        <span className="project-tag">Arduino</span>
                         <span className="project-tag">Python</span>
-                        <span className="project-tag">Flask</span>
-                        <span className="project-tag">OpenAI</span>
-                        <span className="project-tag">Twillio</span>
+                        <span className="project-tag">RFC</span>
+                        <span className="project-tag">React Native</span>
                         <span className="project-tag">MongoDB</span>
-                        <span className="project-tag">NumPy</span>
-                        <span className="project-tag">Pandas</span>
-                        <span className="project-tag">Jupyter</span>
-                        <span className="project-tag">Git</span>
-                        <span className="project-tag">NGROK</span>
                     </div>
                 </div>
             </div>
