8808e42f8149ad775cf6d4ded253caf4a7e62641
edilson-z <dannibuga@gmail.com>
2026-09-24 17:37:46 +0200

    Various image ideas created for project section

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -5,10 +5,12 @@ export default function Projects() {
         <div>
             <h2>Projects</h2>
             <div className="project-cards">
-                <div className="project-card">
-                    <div className='image-container'>
-                        <img src="./sms.png" />
-                    </div>
+                {/* <div className="project-card"> */}
+                {/* <div className='image-container'>
+                        <img src="./sms-3.png" />
+                    </div> */}
+                {/* <img src="./sms-3.png" />
+
                     <div className='project-card-container'>
                         <h3>SMS Agri Chatbot</h3>
                         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.  velit amet culpa repellendus aperiam assumenda quidem vitae</p>
@@ -25,7 +27,68 @@ export default function Projects() {
                     </div>
                 </div>
                 <div className="project-card">
-                    <img src='https://placehold.co/400x200' />
+                    <img src='./sms-2.png' />
+                    <div className='project-card-container'>
+                        <h3>Hydro Tent</h3>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.  velit amet culpa repellendus aperiam assumenda quidem vitae</p>
+                        <span className="project-tag">Java</span>
+                        <span className="project-tag">Kotlin</span>
+                        <span className="project-tag">Spring</span>
+                        <span className="project-tag">Arduino</span>
+                        <span className="project-tag">Python</span>
+                        <span className="project-tag">RFC</span>
+                        <span className="project-tag">React Native</span>
+                        <span className="project-tag">MongoDB</span>
+                    </div>
+                </div> */}
+
+                <div className="project-card">
+                    <img src='./sms-5.png' />
+                    <div className='project-card-container'>
+                        <h3>Hydro Tent</h3>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.  velit amet culpa repellendus aperiam assumenda quidem vitae</p>
+                        <span className="project-tag">Java</span>
+                        <span className="project-tag">Kotlin</span>
+                        <span className="project-tag">Spring</span>
+                        <span className="project-tag">Arduino</span>
+                        <span className="project-tag">Python</span>
+                        <span className="project-tag">RFC</span>
+                        <span className="project-tag">React Native</span>
+                        <span className="project-tag">MongoDB</span>
+                    </div>
+                </div>
+                <div className="project-card">
+                    <img src='./hydro.png' />
+                    <div className='project-card-container'>
+                        <h3>Hydro Tent</h3>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.  velit amet culpa repellendus aperiam assumenda quidem vitae</p>
+                        <span className="project-tag">Java</span>
+                        <span className="project-tag">Kotlin</span>
+                        <span className="project-tag">Spring</span>
+                        <span className="project-tag">Arduino</span>
+                        <span className="project-tag">Python</span>
+                        <span className="project-tag">RFC</span>
+                        <span className="project-tag">React Native</span>
+                        <span className="project-tag">MongoDB</span>
+                    </div>
+                </div>
+                <div className="project-card">
+                    <img src='./jade-3.png' />
+                    <div className='project-card-container'>
+                        <h3>Hydro Tent</h3>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.  velit amet culpa repellendus aperiam assumenda quidem vitae</p>
+                        <span className="project-tag">Java</span>
+                        <span className="project-tag">Kotlin</span>
+                        <span className="project-tag">Spring</span>
+                        <span className="project-tag">Arduino</span>
+                        <span className="project-tag">Python</span>
+                        <span className="project-tag">RFC</span>
+                        <span className="project-tag">React Native</span>
+                        <span className="project-tag">MongoDB</span>
+                    </div>
+                </div>
+                 <div className="project-card">
+                    <img src='./last.png' />
                     <div className='project-card-container'>
                         <h3>Hydro Tent</h3>
                         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.  velit amet culpa repellendus aperiam assumenda quidem vitae</p>
