24de6229b0182c1355b457ec259edebae3694ed9
edilson-z <dannibuga@gmail.com>
2026-09-25 20:26:52 +0200

    Image borders added

--- src/pages/projects/sms.tsx
+++ src/pages/projects/sms.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router";
+// import { Link } from "react-router";
 import Footer from "../../components/footer";
 import Nav from "../../components/nav";
 
@@ -6,7 +6,7 @@ export default function Sms() {
     return (
         <div className="project-shell">
             <Nav />
-            <Link to="/" style={{ all: 'unset', cursor: 'pointer', color: 'var(--main)' }}>Back</Link>
+            {/* <Link to="/" style={{ all: 'unset', cursor: 'pointer', color: 'var(--main)' }}>Back</Link> */}
             <h2>Agri SMS Chatbot</h2>
             <img src="/last.png" className="data-img" />
 
