d2f299854cbcfe75a959eaa4eb0777398f0b7376
edilson-z <dannibuga@gmail.com>
2026-09-25 23:14:06 +0200

    Unused code removed from projects.tsx

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -6,43 +6,6 @@ export default function Projects() {
         <div>
             <h2>Projects</h2>
             <div className="project-cards">
-                {/* <div className="project-card"> */}
-                {/* <div className='image-container'>
-                        <img src="./sms-3.png" />
-                    </div> */}
-                {/* <img src="./sms-3.png" />
-
-                    <div className='project-card-container'>
-                        <h3>SMS Agri Chatbot</h3>
-                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.  velit amet culpa repellendus aperiam assumenda quidem vitae</p>
-                        <span className="project-tag">Python</span>
-                        <span className="project-tag">Flask</span>
-                        <span className="project-tag">OpenAI</span>
-                        <span className="project-tag">Twillio</span>
-                        <span className="project-tag">MongoDB</span>
-                        <span className="project-tag">NumPy</span>
-                        <span className="project-tag">Pandas</span>
-                        <span className="project-tag">Jupyter</span>
-                        <span className="project-tag">Git</span>
-                        <span className="project-tag">NGROK</span>
-                    </div>
-                </div>
-                <div className="project-card">
-                    <img src='./sms-2.png' />
-                    <div className='project-card-container'>
-                        <h3>Hydro Tent</h3>
-                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.  velit amet culpa repellendus aperiam assumenda quidem vitae</p>
-                        <span className="project-tag">Java</span>
-                        <span className="project-tag">Kotlin</span>
-                        <span className="project-tag">Spring</span>
-                        <span className="project-tag">Arduino</span>
-                        <span className="project-tag">Python</span>
-                        <span className="project-tag">RFC</span>
-                        <span className="project-tag">React Native</span>
-                        <span className="project-tag">MongoDB</span>
-                    </div>
-                </div> */}
-
                 <Link to="/projects/hydro" style={{ all: 'unset', cursor: 'pointer' }}>
                     <div className="project-card">
                         <img src='./hydro.png' />
