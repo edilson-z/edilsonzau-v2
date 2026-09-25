3f6da2203e2d5311599bffcf720b415e3b6b8e88
edilson-z <dannibuga@gmail.com>
2026-09-25 16:39:51 +0200

    Footer v1 created. Project card border hover effect removed

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import "./component-styles/footer.css"
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <div className="footer">
+      <p>&copy; 2026 Edilson Zau</p>
+    </div>
   )
 }
