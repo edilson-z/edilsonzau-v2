7451332abc010faa8d0bd98bff08469f586503ca
edilson-z <dannibuga@gmail.com>
2026-09-25 18:56:26 +0200

    Router componets added

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { Routes, Route, BrowserRouter } from 'react-router'
 import Home from './pages/home'
 import Hydro from './pages/projects/hydro'
+import Sms from './pages/projects/sms'
+import Jade from './pages/projects/jade'
 
 function App() {
 
@@ -9,6 +11,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/projects/hydro" element={<Hydro />} />
+        <Route path="/projects/sms" element={<Sms />} />
+        <Route path="/projects/jade" element={<Jade />} />
       </Routes>
     </BrowserRouter>
   )
