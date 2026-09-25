7d45cb875c0de4ac452edc4c466b668df6cf3151
edilson-z <dannibuga@gmail.com>
2026-09-25 19:52:00 +0200

    H3 color fix

--- src/pages/projects/sms.tsx
+++ src/pages/projects/sms.tsx
@@ -1,8 +1,10 @@
+import { Link } from "react-router";
 import Footer from "../../components/footer";
 
 export default function Sms() {
     return (
         <div className="project-shell">
+            <Link to="/" style={{ all: 'unset', cursor: 'pointer' }}>Back</Link>
             <h2>Agri SMS Chatbot</h2>
             <img src="/last.png" className="data-img" />
 
@@ -46,36 +48,36 @@ export default function Sms() {
                 This message prompts the user to participate in the SMS survey for data collection.</p>
 
             <img src="/image9.webp" className="data-img" />
-                <p>Once the client conscents to the survey a flusk session runs for data collection. The diagram
-                    illustrates the
-                    process of conducting an SMS-based survey with rural farmers, from the initial message to the
-                    storage of responses and generation of follow-up questions.</p>
+            <p>Once the client conscents to the survey a flusk session runs for data collection. The diagram
+                illustrates the
+                process of conducting an SMS-based survey with rural farmers, from the initial message to the
+                storage of responses and generation of follow-up questions.</p>
 
-                <h3>Information Sharing Feature</h3>
-                <img src="/image11.webp" className="data-img" />
-                <p>The Information Sharing Component facilitates the distribution of targeted agricultural advice,
-                    market prices, weather updates, and best farming practices through SMS messages.
-                    The diagram shows the farmer sending a message to the chatbot and the chatbot processing it, storing
-                    the data, and generating a response.
-                </p>
+            <h3>Information Sharing Feature</h3>
+            <img src="/image11.webp" className="data-img" />
+            <p>The Information Sharing Component facilitates the distribution of targeted agricultural advice,
+                market prices, weather updates, and best farming practices through SMS messages.
+                The diagram shows the farmer sending a message to the chatbot and the chatbot processing it, storing
+                the data, and generating a response.
+            </p>
 
-                <h3>Data Analytics Feature</h3>
-                <img src="/image25.webp" className="data-img" />
-                <p>The Data Analytics Component processes the collected data to generate actionable insights about
-                    farming patterns, common challenges and needs across rural areas. The diagram
-                    illustrates the data analytics generation process. It begins with querying the database for the
-                    latest data. Then, it proceeds with data preprocessing, where the collected SMS data is cleaned,
-                    standardised, and transformed into a suitable format for analysis.</p>
+            <h3>Data Analytics Feature</h3>
+            <img src="/image25.webp" className="data-img" />
+            <p>The Data Analytics Component processes the collected data to generate actionable insights about
+                farming patterns, common challenges and needs across rural areas. The diagram
+                illustrates the data analytics generation process. It begins with querying the database for the
+                latest data. Then, it proceeds with data preprocessing, where the collected SMS data is cleaned,
+                standardised, and transformed into a suitable format for analysis.</p>
 
-                <h3>Data Dashboards Demo</h3>
-                <img src="/image8.webp" className="data-img" />
-                <img src="/image10.webp" className="data-img" />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <Footer />
+            <h3>Data Dashboards Output</h3>
+            <img src="/image8.webp" className="data-img" />
+            <img src="/image10.webp" className="data-img" />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <Footer />
         </div>
     )
 }
